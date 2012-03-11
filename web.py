ea103a22f3b58778363ddfcc0c707c75c54f4e9c
mahata <nori@mahata.net>
2012-03-10 23:06:46 -0800

    removed test page.

--- web.py
+++ web.py
@@ -130,10 +130,6 @@ def logout():
     return redirect(url_for('index'))
 
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
-
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     app.run(host="0.0.0.0", port=port)
