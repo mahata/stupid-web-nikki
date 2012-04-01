a231114c2b2b69be01374ed5fe58d50ab1bdea91
mahata <nori@mahata.net>
2012-04-01 13:21:58 -0700

    removed ssl_context, since it seems to break heroku.

--- web.py
+++ web.py
@@ -250,4 +250,5 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0',
             port=port,
-            ssl_context=(None if (bool(os.getenv('DEBUG'))) else 'adhoc'))
+            # ssl_context=(None if (bool(os.getenv('DEBUG'))) else 'adhoc'))
+            )
