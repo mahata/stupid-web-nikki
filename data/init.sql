9d859e4395e46b46af7965b0f7fd3389907850b3
Yasunori Mahata <nori@mahata.net>
2014-01-21 20:28:06 -0800

    removed data directory, which is not needed any more.

--- data/init.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS articles;
-CREATE TABLE articles (
-  id SERIAL,
-  article TEXT,
-  created_date INTEGER,
-  UNIQUE(created_date)
-);
-
-ALTER TABLE articles ADD title TEXT;
--- files/ddl.sql
+++ files/ddl.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS articles;
 CREATE TABLE articles (
   id SERIAL,
+  title TEXT,
   article TEXT,
   created_date INTEGER,
   UNIQUE(created_date)
